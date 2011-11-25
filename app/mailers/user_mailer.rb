de6c749f1e32b1c829d427332b131d39579723e8
PedroBJS <esanchez@grupoamsa.com>
2011-11-24 20:16:14 -0500

    Creacion de Mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ActionMailer::Base
+  default from: "eddysz2000@gmail.com"
+
+  def welcom_email(user)
+  	@user = user
+  	@url  = "http://www.gmail.com"
+  	mail(:to=>user.email, :subject=>"Bienvenido")
+  end
+
+end
