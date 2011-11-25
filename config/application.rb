abfa6a85cee242843057040284b10d88ec0613f5
PedroBJS <pedro.jancachagua@gmail.com>
2011-11-25 15:47:34 -0500

    intento de enfocar el item del menu seleccionado con jquery

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module TdewEstacionamiento
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
@@ -45,7 +47,7 @@ module TdewEstacionamiento
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
- 
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
   end
 end
