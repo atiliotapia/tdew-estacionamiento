083ff6cde5d7f114c4c3471399c591578076dfb5
Lennon Shimokawa <lshimokawa@gmail.com>
2011-11-25 15:16:00 -0500

    refactoring usando helpers para limpiar view

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,2 +1,13 @@
 module NavigationHelper
+  
+  def status_image(estacionamiento)
+    if estacionamiento.porcentaje > 66
+  	  "red.png"
+  	elsif estacionamiento.porcentaje < 33
+  		"green.png"
+  	else
+  		"yellow.png"
+  	end
+  end
+  
 end
